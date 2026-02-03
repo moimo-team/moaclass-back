3751860cf787a05ad059b4f20f455cc98f29ce69
jsw4335 <jsw4335@gmail.com>
2026-02-04 03:37:35 +0900

    [feat] ERD 수정에 따른 DB설계 (결제 제외)

--- prisma/migrations/20260201161853_add_curriculum_to_lesson/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `curriculum` to the `lessons` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "lessons" ADD COLUMN     "curriculum" TEXT NOT NULL;
